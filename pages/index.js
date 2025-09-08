8f53ed741b05bd8dbd49856cdafa578b87de4278
jombld11-maker <jombld11@gmail.com>
2025-09-08 21:43:19 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: "center", marginTop: "50px"}}>
+      <h1>Hello 🚀 Comms Dash is Live!</h1>
+      <p>Deployed with GitHub + Vercel (no local setup)</p>
+    </div>
+  )
+}
